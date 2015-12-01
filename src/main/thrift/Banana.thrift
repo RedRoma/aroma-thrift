ff2ec80ae7ff67c4d578277fec8e5bb9d56631d1
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-01 08:24:43 -0800

    Adding Generated Files

---
+++ src/main/thrift/Banana.thrift
@@ -0,0 +1,12 @@
+namespace java  tech.aroma.banana.thrift
+namespace cocoa Banana
+
+typedef i32 int
+typedef i64 long
+
+enum Severity
+{
+    INFORMATIONAL = 1,
+    WARNING = 2,
+    ERROR = 3
+}
