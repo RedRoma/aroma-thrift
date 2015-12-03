5ff1892a4ec7bb15fb833946f4c5db5cec11b21a
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-02 19:34:59 -0800

    Adding New Exceptions

--- src/main/js/Exceptions_types.js
+++ src/main/js/Exceptions_types.js
@@ -59,6 +59,114 @@ InvalidArgumentException.prototype.write = function(output) {
   return;
 };
 
+InvalidCredentialsException = function(args) {
+  this.message = 'Failed to Verify and Authenticate credentials';
+  if (args) {
+    if (args.message !== undefined && args.message !== null) {
+      this.message = args.message;
+    }
+  }
+};
+Thrift.inherits(InvalidCredentialsException, Thrift.TException);
+InvalidCredentialsException.prototype.name = 'InvalidCredentialsException';
+InvalidCredentialsException.prototype.read = function(input) {
+  input.readStructBegin();
+  while (true)
+  {
+    var ret = input.readFieldBegin();
+    var fname = ret.fname;
+    var ftype = ret.ftype;
+    var fid = ret.fid;
+    if (ftype == Thrift.Type.STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+      if (ftype == Thrift.Type.STRING) {
+        this.message = input.readString().value;
+      } else {
+        input.skip(ftype);
+      }
+      break;
+      case 0:
+        input.skip(ftype);
+        break;
+      default:
+        input.skip(ftype);
+    }
+    input.readFieldEnd();
+  }
+  input.readStructEnd();
+  return;
+};
+
+InvalidCredentialsException.prototype.write = function(output) {
+  output.writeStructBegin('InvalidCredentialsException');
+  if (this.message !== null && this.message !== undefined) {
+    output.writeFieldBegin('message', Thrift.Type.STRING, 1);
+    output.writeString(this.message);
+    output.writeFieldEnd();
+  }
+  output.writeFieldStop();
+  output.writeStructEnd();
+  return;
+};
+
+ServiceDoesNotExistException = function(args) {
+  this.message = 'The Specified Service does not exist';
+  if (args) {
+    if (args.message !== undefined && args.message !== null) {
+      this.message = args.message;
+    }
+  }
+};
+Thrift.inherits(ServiceDoesNotExistException, Thrift.TException);
+ServiceDoesNotExistException.prototype.name = 'ServiceDoesNotExistException';
+ServiceDoesNotExistException.prototype.read = function(input) {
+  input.readStructBegin();
+  while (true)
+  {
+    var ret = input.readFieldBegin();
+    var fname = ret.fname;
+    var ftype = ret.ftype;
+    var fid = ret.fid;
+    if (ftype == Thrift.Type.STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+      if (ftype == Thrift.Type.STRING) {
+        this.message = input.readString().value;
+      } else {
+        input.skip(ftype);
+      }
+      break;
+      case 0:
+        input.skip(ftype);
+        break;
+      default:
+        input.skip(ftype);
+    }
+    input.readFieldEnd();
+  }
+  input.readStructEnd();
+  return;
+};
+
+ServiceDoesNotExistException.prototype.write = function(output) {
+  output.writeStructBegin('ServiceDoesNotExistException');
+  if (this.message !== null && this.message !== undefined) {
+    output.writeFieldBegin('message', Thrift.Type.STRING, 1);
+    output.writeString(this.message);
+    output.writeFieldEnd();
+  }
+  output.writeFieldStop();
+  output.writeStructEnd();
+  return;
+};
+
 OperationFailedException = function(args) {
   this.message = 'The Operation could not be completed';
   if (args) {
--- src/main/thrift/Exceptions.thrift
+++ src/main/thrift/Exceptions.thrift
@@ -8,6 +8,16 @@ exception InvalidArgumentException
     1: string message = "Invalid Argument Received"
 }
 
+exception InvalidCredentialsException
+{
+    1: string message = "Failed to Verify and Authenticate credentials"
+}
+
+exception ServiceDoesNotExistException
+{
+    1: string message = "The Specified Service does not exist"
+}
+
 exception OperationFailedException
 {
     1: string message = "The Operation could not be completed"
