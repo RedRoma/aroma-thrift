82cd7bfdbeea3c6dc8db70ef39a7775c06ed9ff5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-25 22:47:04 -0800

    Adding BananaAssertions

---
+++ src/main/java/tech/aroma/banana/thrift/functions/BananaAssertions.java
@@ -0,0 +1,120 @@
+ /*
+  * Copyright 2015 Aroma Tech.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package tech.aroma.banana.thrift.functions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.VerifyTokenRequest;
+import tech.aroma.banana.thrift.authentication.service.VerifyTokenResponse;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.sirwellington.alchemy.annotations.access.NonInstantiable;
+import tech.sirwellington.alchemy.annotations.arguments.NonNull;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
+import tech.sirwellington.alchemy.arguments.ExceptionMapper;
+import tech.sirwellington.alchemy.arguments.FailedAssertionException;
+
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+
+/**
+ * This class contains common Assertions made on Banana Service requests and
+ * other data structures.
+ * 
+ * @author SirWellington
+ */
+@NonInstantiable
+public final class BananaAssertions
+{
+    
+    private final static Logger LOG = LoggerFactory.getLogger(BananaAssertions.class);
+    
+    private BananaAssertions() throws IllegalAccessException
+    {
+        throw new IllegalAccessException("cannot instantiate");
+    }
+    
+    public static ExceptionMapper<InvalidArgumentException> withMessage(String message)
+    {
+        return ex -> new InvalidArgumentException(message);
+    }
+    
+    public static void checkRequestNotNull(Object request) throws InvalidArgumentException
+    {
+        checkNotNull(request, "missing request");
+    }
+    
+    public static void checkNotNull(Object reference, String message) throws InvalidArgumentException
+    {
+        checkThat(reference)
+            .throwing(withMessage(message))
+            .is(notNull());
+    }
+    
+    public static AlchemyAssertion<AuthenticationToken> legalToken()
+    {
+        return t ->
+        {
+            checkThat(t).is(notNull());
+            
+            if (!t.isSet())
+            {
+                throw new FailedAssertionException("Token Has not been set:  " + t);
+            }
+        };
+    }
+    
+    public static AlchemyAssertion<AuthenticationToken> validTokenIn(@NonNull AuthenticationService.Iface authenticationService)
+    {
+        checkThat(authenticationService)
+            .usingMessage("authentication service is null")
+            .is(notNull());
+        
+        return token ->
+        {
+            checkThat(token)
+                .is(legalToken());
+            
+            String tokenId = TokenFunctions.extractTokenId(token);
+            
+            checkThat(tokenId)
+                .usingMessage("tokenId is missing")
+                .is(nonEmptyString());
+            
+            VerifyTokenRequest request = new VerifyTokenRequest()
+                .setTokenId(tokenId);
+            
+            VerifyTokenResponse response;
+            try
+            {
+                response = authenticationService.verifyToken(request);
+            }
+            catch (InvalidTokenException ex)
+            {
+                throw new FailedAssertionException("Token is not valid");
+            }
+            catch (Exception ex)
+            {
+                throw new FailedAssertionException("Could not contact Authentication Service", ex);
+            }
+        };
+    }
+    
+}
---
+++ src/test/java/tech/aroma/banana/thrift/functions/BananaAssertionsTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.thrift.functions;
+
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import tech.aroma.banana.thrift.authentication.ApplicationToken;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.UserToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.VerifyTokenRequest;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
+import tech.sirwellington.alchemy.arguments.ExceptionMapper;
+import tech.sirwellington.alchemy.arguments.FailedAssertionException;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticString;
+import static tech.sirwellington.alchemy.generator.StringGenerators.strings;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(50)
+@RunWith(AlchemyTestRunner.class)
+public class BananaAssertionsTest 
+{
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+    
+    @GeneratePojo
+    private UserToken token;
+    
+    private AuthenticationToken authenticationToken;
+    
+    private String tokenId;
+    
+    @Before
+    public void setUp()
+    {
+        tokenId = token.tokenId;
+        authenticationToken = new AuthenticationToken();
+        authenticationToken.setUserToken(token);
+    }
+    
+
+    @DontRepeat
+    @Test
+    public void testCannotInstantiate() throws Exception
+    {
+        assertThrows(() -> BananaAssertions.class.newInstance())
+            .isInstanceOf(IllegalAccessException.class);
+    }
+    
+    @Test
+    public void testCheckRequestNotNull() throws Exception
+    {
+        
+        assertThrows(() -> BananaAssertions.checkRequestNotNull(null))
+            .isInstanceOf(InvalidArgumentException.class);
+        
+        String string = one(strings());
+        BananaAssertions.checkRequestNotNull(string);
+    }
+
+   
+
+    @Test
+    public void testCheckNotNull() throws Exception
+    {
+        String message = one(alphabeticString());
+        BananaAssertions.checkNotNull(tokenId, message);
+        
+        assertThrows(() -> BananaAssertions.checkNotNull(null, message))
+            .isInstanceOf(InvalidArgumentException.class)
+            .hasMessage(message);
+    }
+
+    @Test
+    public void testWithMessage()
+    {
+        
+        String message = one(strings());
+        ExceptionMapper<InvalidArgumentException> result = BananaAssertions.withMessage(message);
+        
+        InvalidArgumentException ex = result.apply(null);
+        assertThat(ex, notNullValue());
+        assertThat(ex.getMessage(), is(message));
+    }
+
+    @Test
+    public void testLegalToken()
+    {
+        AlchemyAssertion<AuthenticationToken> instance = BananaAssertions.legalToken();
+        assertThat(instance, notNullValue());
+
+        AuthenticationToken authenticationToken = new AuthenticationToken();
+
+        assertThrows(() -> instance.check(authenticationToken))
+            .isInstanceOf(FailedAssertionException.class);
+
+        ApplicationToken applicationToken = one(pojos(ApplicationToken.class));
+        authenticationToken.setApplicationToken(applicationToken);
+        instance.check(authenticationToken);
+        
+        UserToken userToken = one(pojos(UserToken.class));
+        authenticationToken.setUserToken(userToken);
+        instance.check(authenticationToken);
+        
+    }
+
+    @Test
+    public void testValidTokenIn() throws TException
+    {
+        
+        // When Token is good
+        AlchemyAssertion<AuthenticationToken> assertion = BananaAssertions.validTokenIn(authenticationService);
+        assertion.check(authenticationToken);
+        
+        //When token is bad
+        VerifyTokenRequest request = new VerifyTokenRequest()
+        .setTokenId(tokenId);
+        
+        when(authenticationService.verifyToken(request))
+            .thenThrow(new InvalidTokenException());
+        
+        assertThrows(() -> assertion.check(authenticationToken))
+            .isInstanceOf(FailedAssertionException.class);
+    }
+
+}
