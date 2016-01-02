b43ad6b2e466afb0bc1a3bab931f50c926c73e02
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-01 22:12:26 -0800

    Adding new Banana Assertions + validUserTokenIn() + validApplicationTokenIn()

--- src/main/java/tech/aroma/banana/thrift/assertions/BananaAssertions.java
+++ src/main/java/tech/aroma/banana/thrift/assertions/BananaAssertions.java
@@ -1,23 +1,25 @@
- /*
-  * Copyright 2015 Aroma Tech.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package tech.aroma.banana.thrift.assertions;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.authentication.ApplicationToken;
 import tech.aroma.banana.thrift.authentication.AuthenticationToken;
 import tech.aroma.banana.thrift.authentication.UserToken;
 import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
@@ -37,9 +39,8 @@ import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull
 import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
 
 /**
- * This class contains common Assertions made on Banana Service requests and
- * other data structures.
- * 
+ * This class contains common Assertions made on Banana Service requests and other data structures.
+ *
  * @author SirWellington
  */
 @NonInstantiable
@@ -48,7 +49,7 @@ public final class BananaAssertions
     
     private final static Logger LOG = LoggerFactory.getLogger(BananaAssertions.class);
     
-    private BananaAssertions() throws IllegalAccessException
+    BananaAssertions() throws IllegalAccessException
     {
         throw new IllegalAccessException("cannot instantiate");
     }
@@ -83,6 +84,32 @@ public final class BananaAssertions
         };
     }
     
+    public static AlchemyAssertion<ApplicationToken> validApplicationTokenIn(
+        @NonNull AuthenticationService.Iface authenticationService)
+    {
+        checkThat(authenticationService)
+            .usingMessage("authentication service is null")
+            .is(notNull());
+        
+        return token ->
+        {
+            checkThat(token)
+                .usingMessage("token is null")
+                .is(notNull());
+            
+            checkThat(token.tokenId)
+                .usingMessage("token is missing tokenId")
+                .is(nonEmptyString());
+            
+            AuthenticationToken authToken = new AuthenticationToken();
+            authToken.setApplicationToken(token);
+            
+            checkThat(authToken)
+                .is(validTokenIn(authenticationService));
+        };
+        
+    }
+    
     public static AlchemyAssertion<UserToken> validUserTokenIn(@NonNull AuthenticationService.Iface authenticationService)
     {
         checkThat(authenticationService)
@@ -91,10 +118,19 @@ public final class BananaAssertions
         
         return token ->
         {
+            checkThat(token)
+                .usingMessage("token is null")
+                .is(notNull());
+            
+            checkThat(token.tokenId)
+                .usingMessage("token is missing tokenId")
+                .is(nonEmptyString());
+            
             AuthenticationToken authToken = new AuthenticationToken();
             authToken.setUserToken(token);
             
-            validTokenIn(authenticationService).check(authToken);
+            checkThat(authToken)
+                .is(validTokenIn(authenticationService));
         };
     }
     
@@ -140,12 +176,12 @@ public final class BananaAssertions
         {
             return true;
         }
-
+        
         if (token.isSetUserToken())
         {
             return true;
         }
-
+        
         return false;
     }
     
--- src/test/java/tech/aroma/banana/thrift/assertions/BananaAssertionsTest.java
+++ src/test/java/tech/aroma/banana/thrift/assertions/BananaAssertionsTest.java
@@ -1,18 +1,18 @@
-/*
- * Copyright 2015 Aroma Tech.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+  * Copyright 2015 Aroma Tech.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package tech.aroma.banana.thrift.assertions;
 
@@ -40,6 +40,7 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.BooleanGenerators.booleans;
 import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
 import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticString;
 import static tech.sirwellington.alchemy.generator.StringGenerators.strings;
@@ -51,13 +52,19 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  */
 @Repeat(50)
 @RunWith(AlchemyTestRunner.class)
-public class BananaAssertionsTest 
+public class BananaAssertionsTest
 {
+    
     @Mock
     private AuthenticationService.Iface authenticationService;
     
     @GeneratePojo
-    private UserToken token;
+    private ApplicationToken applicationToken;
+    
+    @GeneratePojo
+    private UserToken userToken;
+    
+    private boolean useUserToken;
     
     private AuthenticationToken authenticationToken;
     
@@ -66,16 +73,28 @@ public class BananaAssertionsTest
     @Before
     public void setUp()
     {
-        tokenId = token.tokenId;
         authenticationToken = new AuthenticationToken();
-        authenticationToken.setUserToken(token);
+        
+        useUserToken = one(booleans());
+        if (useUserToken)
+        {
+            authenticationToken.setUserToken(userToken);
+            tokenId = userToken.tokenId;
+        }
+        else
+        {
+            authenticationToken.setApplicationToken(applicationToken);
+            tokenId = applicationToken.tokenId;
+        }
     }
     
-
     @DontRepeat
     @Test
     public void testCannotInstantiate() throws Exception
     {
+        assertThrows(() -> new BananaAssertions())
+            .isInstanceOf(IllegalAccessException.class);
+        
         assertThrows(() -> BananaAssertions.class.newInstance())
             .isInstanceOf(IllegalAccessException.class);
     }
@@ -90,9 +109,7 @@ public class BananaAssertionsTest
         String string = one(strings());
         BananaAssertions.checkRequestNotNull(string);
     }
-
-   
-
+    
     @Test
     public void testCheckNotNull() throws Exception
     {
@@ -103,7 +120,7 @@ public class BananaAssertionsTest
             .isInstanceOf(InvalidArgumentException.class)
             .hasMessage(message);
     }
-
+    
     @Test
     public void testWithMessage()
     {
@@ -115,28 +132,28 @@ public class BananaAssertionsTest
         assertThat(ex, notNullValue());
         assertThat(ex.getMessage(), is(message));
     }
-
+    
     @Test
     public void testLegalToken()
     {
         AlchemyAssertion<AuthenticationToken> instance = BananaAssertions.legalToken();
         assertThat(instance, notNullValue());
-
-        AuthenticationToken authenticationToken = new AuthenticationToken();
-
-        assertThrows(() -> instance.check(authenticationToken))
-            .isInstanceOf(FailedAssertionException.class);
-
-        ApplicationToken applicationToken = one(pojos(ApplicationToken.class));
-        authenticationToken.setApplicationToken(applicationToken);
-        instance.check(authenticationToken);
         
-        UserToken userToken = one(pojos(UserToken.class));
-        authenticationToken.setUserToken(userToken);
-        instance.check(authenticationToken);
+        AuthenticationToken badAuthenticationToken = new AuthenticationToken();
+        
+        assertThrows(() -> instance.check(badAuthenticationToken))
+            .isInstanceOf(FailedAssertionException.class);
+        
+        ApplicationToken badApplicationToken = one(pojos(ApplicationToken.class));
+        badAuthenticationToken.setApplicationToken(badApplicationToken);
+        instance.check(badAuthenticationToken);
+        
+        UserToken badUserToken = one(pojos(UserToken.class));
+        badAuthenticationToken.setUserToken(badUserToken);
+        instance.check(badAuthenticationToken);
         
     }
-
+    
     @Test
     public void testValidTokenIn() throws TException
     {
@@ -145,9 +162,9 @@ public class BananaAssertionsTest
         AlchemyAssertion<AuthenticationToken> assertion = BananaAssertions.validTokenIn(authenticationService);
         assertion.check(authenticationToken);
         
-        //When token is bad
+        //When Token is bad
         VerifyTokenRequest request = new VerifyTokenRequest()
-        .setTokenId(tokenId);
+            .setTokenId(tokenId);
         
         when(authenticationService.verifyToken(request))
             .thenThrow(new InvalidTokenException());
@@ -155,5 +172,41 @@ public class BananaAssertionsTest
         assertThrows(() -> assertion.check(authenticationToken))
             .isInstanceOf(FailedAssertionException.class);
     }
-
+    
+    @Test
+    public void testValidApplicationTokenIn() throws TException
+    {
+        AlchemyAssertion<ApplicationToken> assertion = BananaAssertions.validApplicationTokenIn(authenticationService);
+        assertThat(assertion, notNullValue());
+        
+        //When Token is good
+        assertion.check(applicationToken);
+        
+        //When Token is bad
+        VerifyTokenRequest request = new VerifyTokenRequest(applicationToken.tokenId);
+        when(authenticationService.verifyToken(request))
+            .thenThrow(new InvalidTokenException());
+        
+        assertThrows(() -> assertion.check(applicationToken))
+            .isInstanceOf(FailedAssertionException.class);
+    }
+    
+    @Test
+    public void testValidUserTokenIn() throws TException
+    {
+        AlchemyAssertion<UserToken> assertion = BananaAssertions.validUserTokenIn(authenticationService);
+        assertThat(assertion, notNullValue());
+        
+        //When Token is good
+        assertion.check(userToken);
+        
+        //When Token is bad
+        VerifyTokenRequest request = new VerifyTokenRequest(userToken.tokenId);
+        when(authenticationService.verifyToken(request))
+            .thenThrow(InvalidTokenException.class);
+        
+        assertThrows(() -> assertion.check(userToken))
+            .isInstanceOf(FailedAssertionException.class);
+    }
+    
 }
