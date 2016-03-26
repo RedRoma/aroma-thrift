727a6ae71f0e9048f9dd2a798ddb5d4531b6a79e
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-25 21:18:12 -0700

    Clients: Formatting updates

--- src/main/java/tech/aroma/thrift/services/Clients.java
+++ src/main/java/tech/aroma/thrift/services/Clients.java
@@ -1,18 +1,18 @@
-/*
- * Copyright 2016 Aroma Tech.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+  * Copyright 2016 Aroma Tech.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package tech.aroma.thrift.services;
 
@@ -42,53 +42,53 @@ import tech.sirwellington.alchemy.annotations.access.NonInstantiable;
 @NonInstantiable
 public final class Clients
 {
-
+    
     private final static Logger LOG = LoggerFactory.getLogger(Clients.class);
-
+    
     public static AuthenticationService.Client newAuthenticationServiceClient() throws TTransportException
     {
         TcpEndpoint endpoint = AuthenticationServiceConstants.BETA_ENDPOINT;
-
+        
         TProtocol protocol = tryCreateProtocolAt(endpoint, "Authentication Service");
         return new AuthenticationService.Client(protocol);
     }
-
+    
     public static AuthenticationService.Iface newPerRequestAuthenticationServiceClient() throws TTransportException
     {
         Supplier<AuthenticationService.Iface> clientProvider = () ->
+        {
+            try
             {
-                try
-                {
-                    return newAuthenticationServiceClient();
-                }
-                catch (TTransportException ex)
-                {
-                    LOG.error("Failed to created new Authentication Service client.", ex);
-                    throw new RuntimeException("Could not create Authentication Service client", ex);
-                }
-            };
-
+                return newAuthenticationServiceClient();
+            }
+            catch (TTransportException ex)
+            {
+                LOG.error("Failed to created new Authentication Service client.", ex);
+                throw new RuntimeException("Could not create Authentication Service client", ex);
+            }
+        };
+        
         PerRequestAuthenticationService decorator = new PerRequestAuthenticationService(clientProvider);
-
+        
         return decorator;
     }
-
+    
     public static AromaService.Client newAromaServiceClient() throws TTransportException
     {
         TcpEndpoint endpoint = AromaServiceConstants.BETA_ENDPOINT;
-
+        
         TProtocol protocol = tryCreateProtocolAt(endpoint, "Aroma Service");
         return new AromaService.Client(protocol);
     }
-
+    
     public static NotificationService.Client newNotificationServiceClient() throws TTransportException
     {
         TcpEndpoint endpoint = NotificationServiceConstants.BETA_ENDPOINT;
         TProtocol protocol = tryCreateProtocolAt(endpoint, "Notification Service");
-
+        
         return new NotificationService.Client(protocol);
     }
-
+    
     private static TProtocol tryCreateProtocolAt(TcpEndpoint endpoint, String serviceName) throws TTransportException
     {
         long timeout = TimeUnit.SECONDS.toMillis(45);
@@ -102,7 +102,7 @@ public final class Clients
             LOG.error("Failed to connect to {} at {}", serviceName, endpoint, ex);
             throw ex;
         }
-
+        
         TProtocol protocol = new TBinaryProtocol(transport);
         return protocol;
     }
